7c44791790fe12e0b722b1945953b41c6ed442f0
etteryand0 <etteryand@gmail.com>
2021-03-21 17:27:30 +0900

    Feedback button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,11 +18,35 @@ const Logo = styled.div`
   color: rgba(44, 43, 44, 1);
 `;
 
+const Icons = styled.div`
+  display: flex;
+  width: 18vw;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+const Button = styled.button`
+  all: unset;
+  width: 150px;
+  height: 50px;
+  background-color: #E38C3B;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color:#F8F8F8;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
 const Header = () => {
   return (
     <Headr>
       <Logo>IBY</Logo>
-      <img src={searchIcon} alt="Search" width={36} height={36} />
+      <Icons>
+        <Button>Связь</Button>
+        <img src={searchIcon} alt="Search" width={36} height={36} />
+      </Icons>
     </Headr>
   );
 }
