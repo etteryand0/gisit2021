ef3b2b5ca860bce20a9e9c8c8e1fd79ef5598e5c
etteryand0 <etteryand@gmail.com>
2021-04-06 16:29:48 +0900

    :bug:

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
 from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
-engine = create_engine('sqlite:///businesses.db')
+__dirname = os.path.abspath('.')
+
+path = os.path.join(__dirname, 'business.db')
+
+engine = create_engine(path)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base(cls=DeferredReflection)
